09e51c43baddc04c906d727d62513823b74bd767
YOGESHWARAN TR <109059451+YogeshwaranTR@users.noreply.github.com>
2022-10-12 18:31:58 +0530

    ennamo paniten

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <h1>Github Demo</h1>
+       <h1>Github Demo section</h1>
       </header>
     </div>
   );
